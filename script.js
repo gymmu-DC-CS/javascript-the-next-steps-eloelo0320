b5853f61f4b0e7936930b4c2b457ee230a2f5f0f
dominika <e258610@sbl.ch>
2023-12-07 14:22:23 +0100

    update

--- script.js
+++ script.js
@@ -4,7 +4,8 @@ export function exercise01(args) {
 
   for (let i = 0; i < input.length; i++) {
     const currentElement = input[i]
-    result.push(currentElement)
+    if (currentElement != "e" && currentElement != "E")
+      result.push(currentElement)
   }
   return result.join("")
 }
