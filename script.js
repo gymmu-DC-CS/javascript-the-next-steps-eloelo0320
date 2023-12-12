0e5cdebf634c193f14e357024cc4e66867c5aa46
dominika <e258610@sbl.ch>
2023-12-12 16:31:41 +0100

    on ex5

--- script.js
+++ script.js
@@ -35,3 +35,50 @@ export function exercise03(args) {
   const amount = result.length
   return amount
 }
+
+export function exercise04(args) {
+  const input = args
+  const result = []
+  for (let i = 1; i < input.length; i++) {
+    const currentElement = input[i]
+    const previousElement = input[i - 1]
+    if (currentElement === " " && previousElement != "-") {
+      result.push(currentElement)
+    }
+  }
+  const amount = result.length + 1
+
+  return amount
+}
+
+export function exercise05(args) {
+  const input = args
+  const result = []
+  for (let i = 1; i < input.length; i++) {
+    const currentElement = input[i]
+    if (
+      currentElement.charCodeAt() >= 65 &&
+      currentElement.charCodeAt() <= 90
+    ) {
+      result.push(currentElement)
+    }
+  }
+  if (result != []) {
+    const capital = true
+  }
+  if (capital == true) {
+    return "yes"
+  } else {
+    return "no"
+  }
+}
+
+// if (
+//   (currentElement.charCodeAt() >= 41 &&
+//     currentElement.charCodeAt() <= 57) ||
+//   (currentElement.charCodeAt() >= 72 &&
+//     currentElement.charCodeAt() <= 100) ||
+//   (currentElement.charCodeAt() >= 133 &&
+//     currentElement.charCodeAt() <= 140) ||
+//   (currentElement.charCodeAt() >= 173 && currentElement.charCodeAt() <= 177)
+// ) {
